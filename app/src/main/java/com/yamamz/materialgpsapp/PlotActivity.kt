84a931ee16c7b61d8bbdb20c94991a7eecc85ba8
admin <meleciort1@gmail.com>
2017-10-22 12:12:47 +0800

    merge map api and places api

--- app/src/main/java/com/yamamz/materialgpsapp/PlotActivity.kt
+++ app/src/main/java/com/yamamz/materialgpsapp/PlotActivity.kt
@@ -1,14 +1,18 @@
 package com.yamamz.materialgpsapp
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.Place
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,8 +24,145 @@ import kotlinx.android.synthetic.main.activity_plot.*
 import kotlinx.android.synthetic.main.content_plot.*
 import java.text.DecimalFormat
 import java.util.ArrayList
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import android.text.Html
+import android.text.Spanned
+import android.text.TextUtils
+import android.util.Log
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
 
-class PlotActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class PlotActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionListener {
+    @SuppressLint("SetTextI18n")
+    override fun onPlaceSelected(p0: Place?) {
+
+        val px = resources.getDimensionPixelSize(R.dimen.map_dot_marker_size)
+        val mDotMarkerBitmap = Bitmap.createBitmap(px, px, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(mDotMarkerBitmap)
+        val shape = ContextCompat.getDrawable(this@PlotActivity, R.drawable.circle_marker)
+        shape.setBounds(0, 0, mDotMarkerBitmap.width, mDotMarkerBitmap.height)
+        shape.draw(canvas)
+
+        if (mMap == null) {
+            return
+        }
+        if(points != null){
+
+            tvArea.text="Area: 0.0"
+            AreaOfPolygon=0.0
+            hashMapMarker.clear()
+            //clear all list
+            points?.clear()
+            Eastings.clear()
+            Northings.clear()
+            mMap?.clear()
+
+
+            p0?.latLng?.let { points?.add(it) }
+
+            //add the circle marker to the map
+            marker = mMap?.addMarker(p0?.latLng?.let { MarkerOptions().position(it) }
+                    ?.icon(BitmapDescriptorFactory.fromBitmap(mDotMarkerBitmap))
+                    ?.anchor(0.5f, 0.5f))
+            //put the marker the hashmap for removing purpose
+            marker?.let { points?.size?.minus(0)?.let { it1 -> hashMapMarker.put(it1, it) } }
+
+            //initialize polygon Options
+            val rectOptions = PolygonOptions()
+                    .strokeWidth(4f)
+                    .fillColor(0x7F00FF00)
+                    .add(p0?.latLng)
+            polygon = mMap?.addPolygon(rectOptions)
+
+            //convert wgs84 to UTM projection
+            val convertUtm = CoordinateConversion()
+            val UTM = p0?.latLng?.latitude?.let { p0.latLng?.longitude?.let { it1 -> convertUtm.latLon2UTM(it, it1) } }
+            val lastdot = UTM?.lastIndexOf("-")
+            val E = lastdot?.let { UTM?.substring(0, it) }
+            val N = UTM?.length?.let { lastdot?.plus(1)?.let { it1 -> UTM.substring(it1, it) } }
+            var EastingFormat = java.lang.Double.parseDouble(E)
+            var NorthingFormat = java.lang.Double.parseDouble(N)
+            EastingFormat = Math.round(EastingFormat * 10000.0) / 10000.0
+            NorthingFormat = Math.round(NorthingFormat * 10000.0) / 10000.0
+
+            //add the northing and easting to its list
+            Northings.add(NorthingFormat)
+            Eastings.add(EastingFormat)
+            firstPoint = true
+
+
+
+
+        }
+
+
+
+        else {
+
+            p0?.latLng?.let { points?.add(it) }
+
+            //add the circle marker to the map
+            marker = mMap?.addMarker(p0?.latLng?.let { MarkerOptions().position(it) }
+                    ?.icon(BitmapDescriptorFactory.fromBitmap(mDotMarkerBitmap))
+                    ?.anchor(0.5f, 0.5f))
+            //put the marker the hashmap for removing purpose
+            marker?.let { points?.size?.minus(0)?.let { it1 -> hashMapMarker.put(it1, it) } }
+
+            //initialize polygon Options
+            val rectOptions = PolygonOptions()
+                    .strokeWidth(4f)
+                    .fillColor(0x7F00FF00)
+                    .add(p0?.latLng)
+            polygon = mMap?.addPolygon(rectOptions)
+
+            //convert wgs84 to UTM projection
+            val convertUtm = CoordinateConversion()
+            val UTM = p0?.latLng?.latitude?.let { p0?.latLng?.longitude?.let { it1 -> convertUtm.latLon2UTM(it, it1) } }
+            val lastdot = UTM?.lastIndexOf("-")
+            val E = lastdot?.let { UTM?.substring(0, it) }
+            val N = UTM?.length?.let { lastdot?.plus(1)?.let { it1 -> UTM.substring(it1, it) } }
+            var EastingFormat = java.lang.Double.parseDouble(E)
+            var NorthingFormat = java.lang.Double.parseDouble(N)
+            EastingFormat = Math.round(EastingFormat * 10000.0) / 10000.0
+            NorthingFormat = Math.round(NorthingFormat * 10000.0) / 10000.0
+
+            //add the northing and easting to its list
+            Northings.add(NorthingFormat)
+            Eastings.add(EastingFormat)
+            firstPoint = true
+        }
+
+            val  latLng: LatLng? = p0?.latLng
+
+        async(UI) {
+            animateMapFlyGotoLoc(latLng)
+        }
+    }
+
+    private suspend fun animateMapFlyGotoLoc(loc: LatLng?) {
+        //pause the map so that it will load and sees the flying animation
+        delay(1500)
+        val position = CameraPosition.Builder()
+                .target(loc) // Sets the new camera position
+                .zoom(19f) // Sets the zoom
+                .bearing(0f) // Rotate the camera
+                .tilt(30f)// Set the camera tilt
+                .build()// Creates a CameraPosition from the builder
+        mMap?.animateCamera(CameraUpdateFactory
+                .newCameraPosition(position), 2000, null)
+
+        mMap?.setMaxZoomPreference(19f)//set the maximum zoom level of the map
+
+
+}
+
+
+override fun onError(p0: Status?) {
+
+    }
 
     var mMap:GoogleMap?=null
     private var polygon: Polygon?=null
@@ -120,7 +261,6 @@ class PlotActivity : AppCompatActivity(), OnMapReadyCallback {
 
     }
 
-
     //this function is for calculating the area of a polygon base on northing/easting coordinates
     internal fun calculateArea() {
         if (Northings.size >= 2) {
@@ -162,7 +302,11 @@ class PlotActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plot)
-        setSupportActionBar(toolbar)
+        val autocompleteFragment = fragmentManager.findFragmentById(R.id.autocomplete_fragment) as PlaceAutocompleteFragment
+
+        // Register a listener to receive callbacks when a place has been selected or an error has
+        // occurred.
+        autocompleteFragment.setOnPlaceSelectedListener(this)
 
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
@@ -170,34 +314,32 @@ class PlotActivity : AppCompatActivity(), OnMapReadyCallback {
 
         //undo plotting purpose
         fab.setOnClickListener { view ->
-            if (points?.size ?: 0 > 1) {
+            if (points?.size ?: 0 > 0) {
                 val marker:Marker?= hashMapMarker[points?.size]
                 marker?.remove()
                 hashMapMarker.remove(points?.size)
                 Northings.size.minus(1).let { Northings.removeAt(it) }
                 Eastings.size.minus(1).let { Eastings.removeAt(it) }
                 points?.size?.minus(1)?.let { points?.removeAt(it) }
-                polygon?.points = points
+
 
 
                 //update the area upon deleting corner of a polygon
                 calculateArea()
                 val df=DecimalFormat("##.####")
                 tvArea.text="Area: ${df.format(AreaOfPolygon)}m²"
+
+                if(points?.size==0){
+                    firstPoint=false
+                    tvArea.text="Area: 0.0"
+                    AreaOfPolygon=0.0
+                }
+                else{
+                    polygon?.points = points
+                }
             }
-
             else{
-                //remove the last marker in the map
-                val marker:Marker?= hashMapMarker[1]
-                marker?.remove()
-                hashMapMarker.remove(1)
-                //clear all list
-                points?.clear()
-                Eastings.clear()
-                Northings.clear()
 
-                tvArea.text="Area: 0.0"
-                AreaOfPolygon=0.0
                 Toast.makeText(this, "No recent Plot, Please plot on the map by tapping the map",Toast.LENGTH_SHORT).show()
             }
         }
